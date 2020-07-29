f61b942c687a0903f84ec3a03ea6044e4332ccd4
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-29 20:24:44 +0530

    Add files via upload

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,30 @@
+package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main{
+	
+	public static void main(String[] args) throws Exception {
+		
+		SkillDAO skillDao = new SkillDAO();
+		System.out.println("List of all player is:");
+		List<Skill> list = skillDao.listAllSkills();
+		try {
+			for(Skill s : list)
+			{
+				System.out.println(s.getSkillId()+")"+" "+s.getSkillName());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
